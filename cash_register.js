d45f0aa2e9c54c663687402e8c37cab6ff5e3340
Olivia Clark <oliviaclark27@gmail.com>
2022-06-16 14:04:49 -0400

    Updated

--- cash_register.js
+++ cash_register.js
@@ -1,75 +1,60 @@
 function checkCashRegister(price, cash, cid) {
-    let registerStatus = {
-        open: "OPEN",
-        closed: "CLOSED",
-        insufficient: "INSUFFICIENT_FUNDS"
-      };
-      let change = {status: "", change: cid};
-      let due = cash - price;
-      let cashLeft = cid.reduce((a, b) => a + b[1], 0);
-    
-      if (due === cashLeft) {
-        change.status = registerStatus.closed;
-        change.change = cid;
-      }else if (due > cashLeft) {
-        change.status = registerStatus.insufficient;
-        change.change = [];
-      }else {
-        change.status = registerStatus.open;
-        change.change = calculateChange(due, cid);
-      }
-    
-      function calculateChange(due, inDrawer) {
-        let changeArr = [];
-        let moneyDenomination = {
-          "PENNY": 0.01, 
-          "NICKEL": 0.05, 
-          "DIME": 0.1, 
-          "QUARTER": 0.25, 
-          "ONE": 1, 
-          "FIVE": 5, 
-          "TEN": 10, 
-          "TWENTY": 20, 
-          "ONE HUNDRED": 100
-        };
-    
-        for (let i = inDrawer.length - 1; i >= 0; i--) {
-          let currencyName = inDrawer[i][0];
-          let currencyTotal = inDrawer[i][1];
-          let currencyValue = moneyDenomination[currencyName];
-          let currencyAmount = currencyTotal / currencyValue;
-          let currencyReturned = 0;
-    
-          while (due >= currencyValue && currencyAmount) {
-            due -= currencyValue;
-            currencyAmount--;
-            currencyReturned++;
-          }
-    
-          if (currencyReturned > 0) {
-            changeArr.push([currencyName, currencyReturned * currencyValue]);
-          }
-    
-          if (due === 0.0) {
-            return changeArr;
-          }else if (i === 0) {
-            change.status = registerStatus.insufficient;
-            return [];
-          }else {
-          }
-        }
-      }
-      return change;
-}
+  const registerStatus = {
+    open: "OPEN",
+    closed: "CLOSED",
+    insufficient: "INSUFFICIENT_FUNDS"
+  };
+  let change = {status: null, change: []};
+  //let due = Math.abs(cash - price);
+  let currentSum = 0;
+  let changeArr = [];
+  let cidVar = cid.map(v => [v[0], Math.round(v[1] * 100)]);
+  let priceVar = Math.round(price * 100);
+  let cashVar = Math.round(cash * 100);
+  let dueVar = cashVar - priceVar;
+  let cidVarSum = cidVar.reduce((a, b) => a + b[1], 0);
 
-checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01], 
-    ["NICKEL", 2.05], 
-    ["DIME", 3.1], 
-    ["QUARTER", 4.25], 
-    ["ONE", 90], 
-    ["FIVE", 55], 
-    ["TEN", 20], 
-    ["TWENTY", 60], 
-    ["ONE HUNDRED", 100]
-])
+  const moneyDenomVar = [
+    {curr: "ONE HUNDRED", val: 10000},
+    {curr: "TWENTY", val: 2000},
+    {curr: "TEN", val: 1000},
+    {curr: "FIVE", val: 500},
+    {curr: "ONE", val: 100},
+    {curr: "QUARTER", val: 25},
+    {curr: "DIME", val: 10},
+    {curr: "NICKEL", val: 5},
+    {curr: "PENNY", val: 1}
+  ];
+
+  if (cidVarSum === dueVar) {
+    change.status = registerStatus.closed
+    change.change = cid;
+    return change;
+    //console.log(change);
+  }else if (cidVarSum < dueVar) {
+    change.status = registerStatus.insufficient;
+    return change;
+    //console.log(change);
+  }else {
+    for (let i = 0; i < moneyDenomVar.length; i++) {
+      while (dueVar >= moneyDenomVar[i].val && currentSum < cidVar[8-i][1]) {
+        dueVar -= moneyDenomVar[i].val;
+        currentSum += moneyDenomVar[i].val;
+      }
+      if (currentSum > 0) {
+        changeArr.push([moneyDenomVar[i].curr, currentSum / 100]);
+      } 
+    }
+    if (dueVar === 0) {
+      change.status = registerStatus.open;
+      change.change = changeArr;
+    }else {
+      change.status = registerStatus.insufficient;
+    }
+    return change;
+    //console.log(change); 
+    }
+  }
+
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
