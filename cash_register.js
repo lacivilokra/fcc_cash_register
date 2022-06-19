171e2ba84b5bb80e3dca11fc1fc3f8ad9aca7d8b
Olivia Clark <oliviaclark27@gmail.com>
2022-06-18 20:57:12 -0400

    Updated

--- cash_register.js
+++ cash_register.js
@@ -1,60 +1,50 @@
 function checkCashRegister(price, cash, cid) {
-  const registerStatus = {
-    open: "OPEN",
-    closed: "CLOSED",
-    insufficient: "INSUFFICIENT_FUNDS"
-  };
-  let change = {status: null, change: []};
-  //let due = Math.abs(cash - price);
-  let currentSum = 0;
-  let changeArr = [];
-  let cidVar = cid.map(v => [v[0], Math.round(v[1] * 100)]);
-  let priceVar = Math.round(price * 100);
-  let cashVar = Math.round(cash * 100);
-  let dueVar = cashVar - priceVar;
-  let cidVarSum = cidVar.reduce((a, b) => a + b[1], 0);
+  let newCid = cid.map(v => [v[0], Math.round(v[1] * 100)]);
+  let cash1 = Math.round(cash * 100);
+  let price1 = Math.round(price * 100);
+  let due = cash1 - price1;
+  let total = cid.reduce((a, b) => a + b[1], 0) * 100;
+  let result = {status: null, change: []};
 
-  const moneyDenomVar = [
-    {curr: "ONE HUNDRED", val: 10000},
-    {curr: "TWENTY", val: 2000},
-    {curr: "TEN", val: 1000},
-    {curr: "FIVE", val: 500},
-    {curr: "ONE", val: 100},
-    {curr: "QUARTER", val: 25},
-    {curr: "DIME", val: 10},
-    {curr: "NICKEL", val: 5},
-    {curr: "PENNY", val: 1}
-  ];
-
-  if (cidVarSum === dueVar) {
-    change.status = registerStatus.closed
-    change.change = cid;
-    return change;
-    //console.log(change);
-  }else if (cidVarSum < dueVar) {
-    change.status = registerStatus.insufficient;
-    return change;
-    //console.log(change);
+  if (due > total) {
+    result.status = "INSUFFICIENT_FUNDS";
+    return result;
+  }else if (due === total) {
+    result.status = "CLOSED";
+    result.change = [...cid];
+    return result;
   }else {
-    for (let i = 0; i < moneyDenomVar.length; i++) {
-      while (dueVar >= moneyDenomVar[i].val && currentSum < cidVar[8-i][1]) {
-        dueVar -= moneyDenomVar[i].val;
-        currentSum += moneyDenomVar[i].val;
-      }
-      if (currentSum > 0) {
-        changeArr.push([moneyDenomVar[i].curr, currentSum / 100]);
-      } 
+    let changeArr = [];
+    const denom = [
+        {curr: "ONE HUNDRED", val: 10000},
+        {curr: "TWENTY", val: 2000},
+        {curr: "TEN", val: 1000},
+        {curr: "FIVE", val: 500},
+        {curr: "ONE", val: 100},
+        {curr: "QUARTER", val: 25},
+        {curr: "DIME", val: 10},
+        {curr: "NICKEL", val: 5},
+        {curr: "PENNY", val: 1}
+      ];
+    for (let i = 0; i < denom.length; i++) {
+        let currentVal = 0;
+        while (due >= denom[i].val && currentVal < newCid[8 - i][1]) {
+            due -= denom[i].val;
+            currentVal += denom[i].val;
+        }
+        if (currentVal > 0) {
+            changeArr.push([denom[i].curr, currentVal / 100]);
+        }
     }
-    if (dueVar === 0) {
-      change.status = registerStatus.open;
-      change.change = changeArr;
+    if (due === 0) {
+        result.status = "OPEN";
+        result.change = changeArr;
     }else {
-      change.status = registerStatus.insufficient;
+        result.status = "INSUFFICIENT_FUNDS";
     }
-    return change;
-    //console.log(change); 
-    }
+    return result;
   }
+}
 
 
 checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
